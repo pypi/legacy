19e53245b36d17daccba0606ef36a9fcd1e8d6af
richard <none@none>
2005-03-19 21:18:12 +0000

    database migration  / creation tools

---
+++ tools/migrate.py
@@ -0,0 +1,37 @@
+# migrate a historical SQLITE database to a postgres one
+
+import sqlite
+import psycopg
+
+old = sqlite.connect(db='pkgbase.db')
+cursor = old.cursor()
+e = cursor.execute
+
+new = psycopg.connect(database='pypi')
+new_cursor = new.cursor()
+f = new_cursor.execute
+
+t = [
+ 'users name password email public_key',
+ 'packages name stable_version',
+ 'releases name version author author_email maintainer maintainer_email home_page license summary description keywords platform download_url _pypi_ordering _pypi_hidden',
+ 'trove_classifiers id classifier',
+ 'release_classifiers name version trove_id',
+ 'journals name version action submitted_date submitted_by submitted_from',
+ 'rego_otk name otk',
+ 'roles role_name user_name package_name',
+]
+for table in t:
+    l = table.split()
+    tn = l[0]
+    print tn
+    cols = ', '.join(l[1:])
+    args = ', '.join(['%s']*(len(l)-1))
+    e('select %(cols)s from %(tn)s'%locals())
+    for row in cursor.fetchall():
+        d = list(row)
+        if '_pypi_ordering' in cols: d[-2] = d[-2] and int(float(d[-2]))
+        f('insert into %(tn)s (%(cols)s) values (%(args)s)'%locals(),
+            tuple(d))
+
+new.commit()
---
+++ tools/sqlite_create.py
@@ -0,0 +1,115 @@
+# HISTORICAL USE ONLY
+
+# IS THE CODE THAT USED TO CONSTRUCT THE SQLITE DATABASE
+
+# IS NOT COMPLETE
+
+# HISTORICAL USE ONLY
+cursor.execute('''
+    create table ids (
+        name varchar,
+        num varchar
+    )''')
+cursor.execute('''
+    create table packages (
+        name varchar,
+        stable_version varchar
+    )''')
+cursor.execute('''
+    create table releases (
+        name varchar,
+        version varchar,
+        author varchar,
+        author_email varchar,
+        maintainer varchar,
+        maintainer_email varchar,
+        home_page varchar,
+        license varchar,
+        summary varchar,
+        description varchar,
+        keywords varchar,
+        platform varchar,
+        download_url varchar,
+        _pypi_ordering varchar,
+        _pypi_hidden varchar
+    )''')
+cursor.execute('''
+    create table trove_classifiers (
+        id varchar,
+        classifier varchar
+    )''')
+cursor.execute('''
+    create table release_classifiers (
+        name varchar,
+        version varchar,
+        trove_id varchar
+    )''')
+cursor.execute('''
+    create table journals (
+        name varchar,
+        version varchar,
+        action varchar,
+        submitted_date varchar,
+        submitted_by varchar,
+        submitted_from varchar
+    )''')
+cursor.execute('''
+    create table users (
+        name varchar,
+        password varchar,
+        email varchar,
+        public_key varchar
+    )''')
+cursor.execute('''
+    create table rego_otk (
+        name varchar,
+        otk varchar
+    )''')
+cursor.execute('''
+    create table roles (
+        role_name varchar,
+        user_name varchar,
+        package_name varchar
+    )''')
+
+# init the id counter
+cursor.execute('''insert into ids (name, num) values
+    ('trove_classifier', 1)''')
+
+# indexes
+SQLs = [
+"create index ids_name_idx on ids(name)",
+"create index journals_name_idx on journals(name)",
+"create index journals_version_idx on journals(version)",
+"create index packages_name_idx on packages(name)",
+"create index rego_otk_name_idx on rego_otk(name)",
+"create index rel_class_name_idx on release_classifiers(name)",
+"create index rel_class_trove_id_idx on "
+    "release_classifiers(trove_id)",
+"create index rel_class_version_id_idx on "
+    "release_classifiers(version)",
+"create index release_name_idx on releases(name)",
+"create index release_pypi_hidden_idx on releases(_pypi_hidden)",
+"create index release_version_idx on releases(version)",
+"create index roles_pack_name_idx on roles(package_name)",
+"create index roles_user_name_idx on roles(user_name)",
+"create index trove_class_class_idx on "
+    "trove_classifiers(classifier)",
+"create index trove_class_id_idx on trove_classifiers(id)",
+"create index users_email_idx on users(email)",
+"create index users_name_idx on users(name)",
+]
+for sql in SQLs:
+    cursor.execute(sql)
+
+# admin user
+adminpw = ''.join([random.choice(chars) for x in range(10)])
+adminpw = sha.sha(adminpw).hexdigest()
+cursor.execute('''
+    insert into users (name, password, email) values
+    ('admin', '%s', NULL)
+    '''%adminpw)
+cursor.execute('''
+    insert into roles (user_name, role_name, package_name) values
+    ('admin', 'Admin', NULL)
+    ''')
