057de4dc36e3b9ad7dc5e8547e541013aeb57e6b
martin.von.loewis <none@none>
2007-07-21 19:08:14 +0000

    Suppress self-referential URLs in simple UI.

--- webui.py
+++ webui.py
@@ -427,6 +427,9 @@ class WebUI:
                         % path)
             html.append("<body><h1>Links for %s</h1>" % path)
             for href, rel, text in urls:
+                if href.startswith('http://cheeseshop.python.org/pypi'):
+                    # Suppress URLs that point to us
+                    continue
                 if rel:
                     rel = ' rel="%s"' % rel
                 else:
