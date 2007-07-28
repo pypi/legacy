13430a874b2facb2f428f834e6d66b6caeb8fba6
martin.von.loewis <none@none>
2007-07-28 07:33:23 +0000

    Document post-update store.py invocations.

--- tools/sql-migrate-20070706.sql
+++ tools/sql-migrate-20070706.sql
@@ -4,3 +4,5 @@ ALTER TABLE trove_classifiers ADD COLUMN l4 INTEGER;
 ALTER TABLE trove_classifiers ADD COLUMN l5 INTEGER;
 
 CREATE INDEX rel_class_name_version_idx ON release_classifiers(name, version);
+
+-- run 'store.py config.ini checktrove' after installing this change
--- tools/sql-migrate-20070721.sql
+++ tools/sql-migrate-20070721.sql
@@ -7,3 +7,5 @@ CREATE TABLE description_urls (
 CREATE INDEX description_urls_name_idx ON description_urls(name);
 CREATE INDEX description_urls_name_version_idx ON description_urls(name, version);
 grant all on description_urls to pypi;
+
+-- run 'store.py config.ini updateurls' after installing this change
