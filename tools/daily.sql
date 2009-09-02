acad2852f4073551f9ea7e9c1f61a4f29b533fa6
martin.von.loewis <none@none>
2009-09-02 06:54:16 +0000

    Invert incorrect test.

--- tools/daily.sql
+++ tools/daily.sql
@@ -1,3 +1,3 @@
 -- run as a cronjob: psql packages -f tools/daily.sql -o /dev/null
 delete from cookies where last_seen < now()-INTERVAL'1day';
-delete from openid_sessions where expires > now();
+delete from openid_sessions where expires < now();
