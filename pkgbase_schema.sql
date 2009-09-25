153919c42f69eb9a5b59a02b1c675afd06cd55b9
martin.von.loewis <none@none>
2009-09-25 16:32:49 +0000

    Copy ratings table from tools script.

--- pkgbase_schema.sql
+++ pkgbase_schema.sql
@@ -283,3 +283,16 @@ CREATE TABLE mirrors (
    user_name TEXT REFERENCES users
 );
 
+-- ratings
+CREATE TABLE ratings(
+   name TEXT,
+   version TEXT,
+   user_name TEXT REFERENCES users ON DELETE CASCADE,
+   date TIMESTAMP,
+   rating INTEGER,
+   message TEXT,
+   PRIMARY KEY (name, version, user_name),
+   FOREIGN KEY (name, version) REFERENCES releases ON UPDATE CASCADE ON DELETE CASCADE
+);
+CREATE INDEX rating_name_version ON ratings(name, version);
+
