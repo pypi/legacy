c26111a037af3d840b68bbe6d8f98b8af3d958ca
martin.von.loewis <none@none>
2010-01-02 14:28:43 +0000

    Support interaction over stdin/out.

--- standalone.py
+++ standalone.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-import config, webui, BaseHTTPServer, urllib
+import config, webui, BaseHTTPServer, urllib, sys, getopt
 
 class RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
     config = config.Config("config.ini")
@@ -25,11 +25,11 @@ class RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
             query = ''            
 
         env = {}
-        env['SERVER_SOFTWARE'] = self.version_string()
-        env['SERVER_NAME'] = self.server.server_name
+        #env['SERVER_SOFTWARE'] = self.version_string()
+        #env['SERVER_NAME'] = self.server.server_name
+        #env['SERVER_PORT'] = str(self.server.server_port)
         env['GATEWAY_INTERFACE'] = 'CGI/1.1'
         env['SERVER_PROTOCOL'] = self.protocol_version
-        env['SERVER_PORT'] = str(self.server.server_port)
         env['REQUEST_METHOD'] = self.command
         uqrest = urllib.unquote(rest)
         env['PATH_INFO'] = uqrest
@@ -84,5 +84,31 @@ class RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
         webui.WebUI(self, env).run()
     do_GET = do_POST = run
 
-httpd = BaseHTTPServer.HTTPServer(('',8000), RequestHandler)
-httpd.serve_forever()
+class StdinoutHandler(RequestHandler):
+    def __init__(self):
+        # request, client_address, server
+        RequestHandler.__init__(self, None, ('',0), None)
+    def setup(self):
+        self.rfile = sys.stdin
+        #import StringIO
+        #self.rfile = StringIO.StringIO('GET /pypi HTTP/1.0\r\n\r\n')
+        self.wfile = sys.stdout
+
+def main():
+    port = 8000
+    opts, args =  getopt.getopt(sys.argv[1:], 'ip:',
+                                ['interactive', 'port='])
+    assert not args
+    for opt, val in opts:
+        if opt in ('-i', '--interactive'):
+            port = None
+        elif opt in ('-p', '--port'):
+            port = int(val)
+    if port:
+        httpd = BaseHTTPServer.HTTPServer(('',8000), RequestHandler)
+        httpd.serve_forever()
+    else:
+        StdinoutHandler()
+
+if __name__=='__main__':
+    main()
