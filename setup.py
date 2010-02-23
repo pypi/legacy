fa670f98d72c98110f58ce215f5f3ee0f9370880
richard <none@none>
2010-02-23 03:36:58 +0000

    add more useful information

--- setup.py
+++ setup.py
@@ -14,10 +14,13 @@ setup(
 <http://pypi.python.org/>. Users should need not need to change
 anything, as the old "www" address should still work. Use of the new
 address in preference is encouraged.
+
+Developers interested in how PyPI works, or in contributing to the project,
+should visit http://wiki.python.org/moin/CheeseShopDev
 ''',
     author = "Richard Jones",
-    author_email = "richard@mechanicalcat.net",
-    url = 'http://pypi.python.org/',
+    author_email = "richard@python.org",
+    url = 'http://wiki.python.org/moin/CheeseShopDev',
     classifiers = [
         'Development Status :: 5 - Production/Stable',
         'Environment :: Web Environment',
