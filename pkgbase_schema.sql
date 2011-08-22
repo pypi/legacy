8e699a3be0febe2b7bc9fb2d51b2a7f73d93c8db
richard <none@none>
2011-08-22 05:38:18 +0000

    hmm, the most recent migrate with the referenced columns removes them, even though they are in the production schema...

--- pkgbase_schema.sql
+++ pkgbase_schema.sql
@@ -244,13 +244,7 @@ CREATE TABLE browse_tally (
 -- Table structure for table: mirrors
 CREATE TABLE mirrors (
    ip TEXT PRIMARY KEY,
-   user_name TEXT REFERENCES users,
-   index_url TEXT,
-   last_modified_url TEXT,
-   local_stats_url TEXT,
-   stats_url TEXT,
-   mirrors_url TEXT
-   
+   user_name TEXT REFERENCES users
 );
 
 -- ratings
