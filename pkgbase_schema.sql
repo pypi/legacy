c6d018179d0bcbb09d52cce7644ee8f9ebeabef6
martin.von.loewis <none@none>
2011-11-20 20:16:39 +0000

    Add missing changes.

--- pkgbase_schema.sql
+++ pkgbase_schema.sql
@@ -26,19 +26,11 @@ CREATE TABLE openid_discovered (
 
 CREATE TABLE openid_sessions (
    id SERIAL PRIMARY KEY,
-   provider TEXT,
    url TEXT,
    assoc_handle TEXT,
    expires TIMESTAMP,
    mac_key TEXT
 );
-
-CREATE TABLE openid_stypes (
-   id INTEGER REFERENCES openid_sessions ON DELETE CASCADE,
-   stype TEXT
-);
-CREATE INDEX openid_stypes_id ON openid_stypes(id);
-
 
 CREATE TABLE openid_nonces (
    created TIMESTAMP,
