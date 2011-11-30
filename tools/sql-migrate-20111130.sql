ec4085e3508ae324e227c1038bebcf37300d7371
martin.von.loewis <none@none>
2011-11-30 09:30:39 +0000

    Support renaming users for roles.

--- tools/sql-migrate-20111130.sql
+++ tools/sql-migrate-20111130.sql
@@ -2,3 +2,7 @@ begin;
 alter table journals drop constraint "$1";
 alter table journals add foreign key (submitted_by) references users (name) on update cascade;
 end;
+begin;
+alter table roles drop constraint "$1";
+alter table roles add foreign key (user_name) references users (name) on update cascade;
+end;
