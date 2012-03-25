3ee36a512b2000f0e5171ece54036d3c8f68f460
Martin v. Löwis <martin@v.loewis.de>
2012-03-25 18:36:14 +0200

    Replace varchar2 with varchar. Closes #3 on https://bitbucket.org/loewis/pypi.

--- tools/sql-migrate-20120314.sql
+++ tools/sql-migrate-20120314.sql
@@ -1,27 +1,27 @@
 -- tables for the oauth library
 
 CREATE TABLE oauth_consumers (
-      consumer              varchar2(32) primary key,
-      secret                varchar2(64) not null,
+      consumer              varchar(32) primary key,
+      secret                varchar(64) not null,
       date_created          date not null,
       created_by TEXT REFERENCES users ON UPDATE CASCADE,
       last_modified         date not null,
-      description           varchar2(255) not null
+      description           varchar(255) not null
 );
 
 CREATE TABLE oauth_request_tokens (
-      token                 varchar2(32) primary key,
-      secret                varchar2(64) not null,
-      consumer              varchar2(32) not null,
+      token                 varchar(32) primary key,
+      secret                varchar(64) not null,
+      consumer              varchar(32) not null,
       callback              text,
       date_created          date not null,
       user TEXT REFERENCES users ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 CREATE TABLE oauth_access_tokens (
-      token                 varchar2(32) primary key,
-      secret                varchar2(64) not null,
-      consumer              varchar2(32) not null,
+      token                 varchar(32) primary key,
+      secret                varchar(64) not null,
+      consumer              varchar(32) not null,
       date_created          date not null,
       last_modified         date not null,
       user TEXT REFERENCES users ON UPDATE CASCADE ON DELETE CASCADE
@@ -29,8 +29,8 @@ CREATE TABLE oauth_access_tokens (
 
 CREATE TABLE oauth_nonce (
       timestamp             integer not null,
-      consumer              varchar2(32) not null,
-      nonce                 varchar2(32) not null,
-      token                 varchar2(32)
+      consumer              varchar(32) not null,
+      nonce                 varchar(32) not null,
+      token                 varchar(32)
 );
 
