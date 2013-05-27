784d12d0da3cfc2d6ead22f7162f3bfd5c93ffa6
Donald Stufft <donald@stufft.io>
2013-05-27 13:30:38 -0400

    Fix for using the proper name

--- store.py
+++ store.py
@@ -2315,7 +2315,7 @@ class Store:
         if self.config.fastly_api_key:
             session = requests.session()
             for package in self._changed_packages:
-                normalized_name = normalize_package_name(name)
+                normalized_name = normalize_package_name(package)
                 path = "/service/%(id)s/purge/pkg~%(name)s" % {"id": None, "name": normalized_name}
                 url = urlparse.urljoin(self.config.fastly_api_domain, path)
                 session.post(url,
