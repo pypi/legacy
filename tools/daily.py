4c0d9b5b4647a5b9388dcf23f3094a1cea1b365e
Donald Stufft <donald@stufft.io>
2014-01-25 16:31:08 -0500

    Add a daily.py script

---
+++ tools/daily.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+import sys
+import os
+
+prefix = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, prefix)
+
+# Workaround current bug in docutils:
+# http://permalink.gmane.org/gmane.text.docutils.devel/6324
+import docutils.utils
+
+import config
+import store
+
+CONFIG_FILE = os.environ.get("PYPI_CONFIG", os.path.join(prefix, 'config.ini'))
+
+conf = config.Config(CONFIG_FILE)
+store = store.Store(conf)
+
+cursor = store.get_cursor()
+
+cursor.execute("delete from cookies where last_seen < now()-INTERVAL'1day';")
+cursor.execute("delete from openid_sessions where expires < now();")
+cursor.execute("delete from openid_nonces where created < now()-INTERVAL'1day'; ")
+cursor.execute("delete from openids where name in (select name from rego_otk where date < now()-INTERVAL'7days');")
+cursor.execute("delete from users where name in (select name from rego_otk where date < now()-INTERVAL'7days' and name not in (select user_name from roles));")
+
+store.commit()
