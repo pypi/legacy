de311a89bb4c01f001beeeb1fa40e5ac17079dfb
Donald Stufft <donald@stufft.io>
2014-04-13 15:24:24 -0400

    Handle redirects properly

--- webui.py
+++ webui.py
@@ -875,6 +875,11 @@ class WebUI:
 
     def packages(self):
         path = self.env.get('PATH_INFO')
+        parts = path.split("/")
+
+        elif len(parts) < 5:
+            if not path.endswith("/"):
+                raise Redirect(path + "/")
 
         # I expect that nginx will do the right thing if it doesn't find the
         # actual file when resolving the X-accel headers.
