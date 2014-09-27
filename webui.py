089e812a7d9a23c21338aa6a78ae57e8f374588a
Ernest W. Durbin III <ewdurbin@gmail.com>
2014-09-27 22:18:31 +0000

    add route for obtaining current serial

--- webui.py
+++ webui.py
@@ -591,6 +591,8 @@ class WebUI:
             return self.security()
         if script_name == '/daytime':
             return self.daytime()
+        if script_name == '/serial':
+            return self.current_serial()
         if script_name == '/id':
             return self.run_id()
 
@@ -3299,6 +3301,14 @@ class WebUI:
         options = {'title': 'PyPI Security'}
         self.write_template('security.pt', **options)
 
+    def current_serial(self):
+        # Provide an endpoint for quickly determining the current serial
+        self.handler.send_response(200, 'OK')
+        self.handler.set_content_type('text/plain')
+        self.handler.end_headers()
+        serial = self.store.changelog_last_serial() or 0
+        self.wfile.write(str(serial))
+        
     def daytime(self):
         # Mirrors are supposed to provide /last-modified,
         # but it doesn't make sense to do so for the master server
