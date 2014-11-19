a78e9d62d24e1fa9d7c4df2a2fdb10869d1949c2
Richard Jones <richard@mechanicalcat.net>
2014-11-19 15:11:19 +1100

    missed some

--- webui.py
+++ webui.py
@@ -2071,6 +2071,9 @@ class WebUI:
         name = data['name']
         version = data['version']
 
+        if name.lower() in ('requirements.txt', 'rrequirements.txt'):
+            raise Forbidden, "Package name '%s' invalid" % name
+
         # don't hide by default
         if not data.has_key('_pypi_hidden'):
             data['_pypi_hidden'] = '0'
@@ -2331,6 +2334,9 @@ class WebUI:
         if self.form.has_key('submit_remove'):
             return self.remove_pkg()
 
+        if name.lower() in ('requirements.txt', 'rrequirements.txt'):
+            raise Forbidden, "Package name '%s' invalid" % name
+
         # make sure the user has permission to do stuff
         if not (self.store.has_role('Owner', name) or
                 self.store.has_role('Admin', name) or
@@ -2586,6 +2592,9 @@ class WebUI:
             version = parameters['version']
         if not name or not version:
             raise FormError, 'Name and version are required'
+
+        if name.lower() in ('requirements.txt', 'rrequirements.txt'):
+            raise Forbidden, "Package name '%s' invalid" % name
 
         # make sure the user has permission to do stuff
         if not (self.store.has_role('Owner', name) or
