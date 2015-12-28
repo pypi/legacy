8109829fbd4f8508095245bccaada06d07065f5e
Ernest W. Durbin III <ewdurbin@gmail.com>
2015-12-28 13:12:22 -0500

    crack at a form based login in the interim to warehouse

--- webui.py
+++ webui.py
@@ -798,7 +798,7 @@ class WebUI:
         file_upload show_md5 doc_upload claim openid openid_return dropid
         clear_auth addkey delkey lasthour json gae_file about delete_user
         rss_regen openid_endpoint openid_decide_post packages_rss
-        exception'''.split():
+        exception login_form'''.split():
             getattr(self, action)()
         else:
             #raise NotFound, 'Unknown action %s' % action
@@ -810,49 +810,68 @@ class WebUI:
         # commit any database changes
         self.store.commit()
 
-    def _handle_basic_auth(self, auth):
-        if not auth.lower().startswith('basic '):
-            return
+    def _check_credentials(self, username, password):
 
-        authtype, auth = auth.split(None, 1)
-        try:
-            un, pw = base64.decodestring(auth).split(':', 1)
-        except (binascii.Error, ValueError):
-            # Invalid base64, or no colon
-            un = pw = ''
-        if not self.store.has_user(un):
+        if not self.store.has_user(username):
             raise UserNotFound
 
-        if self._check_blocked_user(un):
-            un = pw = ''
+        if self._check_blocked_user(username):
+            username = password = ''
             raise UserNotFound
 
 
         # Fetch the user from the database
-        user = self.store.get_user(un)
+        user = self.store.get_user(username)
 
         # Verify the hash, and see if it needs migrated
-        ok, new_hash = self.config.passlib.verify_and_update(pw, user["password"])
+        ok, new_hash = self.config.passlib.verify_and_update(password, user["password"])
 
         # If our password didn't verify as ok then raise an
         #   error.
         if not ok:
-            self._failed_login_user(un)
+            self._failed_login_user(username)
             raise Unauthorised, 'Incorrect password'
 
         if new_hash:
             # The new hash needs to be stored for this user.
-            self.store.setpasswd(un, new_hash, hashed=True)
+            self.store.setpasswd(username, new_hash, hashed=True)
 
         # Login the user
-        self.username = un
+        self.username = username
         self.authenticated = True
 
         # Determine if we need to store the users last login,
         #   as we only want to do this once a minute.
         last_login = user['last_login']
         update_last_login = not last_login or (time.time()-time.mktime(last_login.timetuple()) > 60)
-        self.store.set_user(un, self.remote_addr, update_last_login)
+        self.store.set_user(username, self.remote_addr, update_last_login)
+
+    def _handle_basic_auth(self, auth):
+        if not auth.lower().startswith('basic '):
+            return
+
+        authtype, auth = auth.split(None, 1)
+        try:
+            username, password = base64.decodestring(auth).split(':', 1)
+        except (binascii.Error, ValueError):
+            # Invalid base64, or no colon
+            username = password = ''
+
+        self._check_credentials(username, password)
+
+    def login_form(self):
+        if self.env['REQUEST_METHOD'] == "POST":
+            self.csrf_check()
+
+            username = self.form.get('username', '')
+            password = self.form.get('password', '')
+
+            self._check_credentials(username, password)
+        elif self.env['REQUEST_METHOD'] == "GET":
+            self.write_template('login.pt', title="PyPI Login")
+        else:
+            self.handler.send_response(405, 'Method Not Allowed')
+            
 
     def _failed_login_ip(self):
         if self.block_redis:
